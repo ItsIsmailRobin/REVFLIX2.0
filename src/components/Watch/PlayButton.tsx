cec92ee62554ed1d9f7921ecbb49b950c9a29844
Revenger_xD <87826486+ItsIsmailRobin@users.noreply.github.com>
2024-08-27 09:15:26 +0600

    Update PlayButton.tsx

--- src/components/Watch/PlayButton.tsx
+++ src/components/Watch/PlayButton.tsx
@@ -111,6 +111,7 @@ const PlayButton = ({
           )}
         </button>
   <p className="text-[#efe8fa] text-xs" >
+<P> </P>
           <b>REVFLIX</b> Developed By <b>Revenger</b>
         </p> 
       </div>
